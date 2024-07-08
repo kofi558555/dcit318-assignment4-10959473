f35a7062bdf4d135128d6f24b1e22601076f8f57
pkappiah014@st.ug.edu.gh <102973954+kofi558555@users.noreply.github.com>
2024-07-08 04:36:58 -0700

    Add files via upload
    2nd commit

---
+++ AddressBook/AddressBook/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddressBook
+{
+    public partial class Contact : Form
+    {
+        public Contact()
+        {
+            InitializeComponent();
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            string name = nameField.Text;
+            string email = emailField.Text;
+            string phoneNumber = phoneNumberField.Text;
+
+            List<String> address = new List<string> { name, email, phoneNumber };   // Store details in address list
+
+            List<List<String>> addressBook = new List<List<String>>();    // Append the address list to addressBook list
+            addressBook.Add(address); // Add address to address book
+            address.Clear(); // Clear address list 
+
+            MessageBox.Show($"Details: {name}, {email}, {phoneNumber}\nContact Saved.");
+
+        }
+    }
+}
